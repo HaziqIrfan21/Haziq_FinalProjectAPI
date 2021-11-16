1a0658382a4394255142a5942bf51784381e7b1c
HaziqIrfan21 <90903576+HaziqIrfan21@users.noreply.github.com>
2021-11-17 00:15:08 +0800

    added php for item

--- RestaurantAPI/Controllers/MainController.cs
+++ RestaurantAPI/Controllers/MainController.cs
@@ -4,6 +4,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using DatabaseSQL;
+using Haziq_FinalProject;
 using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
 using Newtonsoft.Json;
@@ -12,7 +15,7 @@ using Newtonsoft.Json;
 
 namespace RestaurantAPI.Controllers
 {
-    [Route("api/main")]
+    [Route("api/mainmenu")]
     [ApiController]
     public class MainController : ControllerBase
     {
@@ -48,6 +51,76 @@ namespace RestaurantAPI.Controllers
         {
         }
 
-        
+        [Route("AddToOrder")]
+        [HttpPut]
+        public void AddToOrder(string userName, string itemName, int qty, decimal price, decimal total)
+        {
+
+            //---------//
+
+            //Add new user
+            DBAPI db = new DBAPI();
+            MySqlCommand command = new MySqlCommand("INSERT INTO `orders` (`UserName`,`ItemName`, `Qty`, `Price`, `TotalPrice`) VALUES (@nme,@itn, @qty, @price, @total_price)", db.GetConnection());
+
+           
+
+
+            // command.Parameters.Add("@usn", MySqlDbType.VarChar).Value = GetUser();
+            command.Parameters.Add("@nme", MySqlDbType.VarChar).Value = userName;
+            command.Parameters.Add("@itn", MySqlDbType.VarChar).Value = itemName;
+            command.Parameters.Add("@qty", MySqlDbType.Int32).Value = qty;
+            command.Parameters.Add("@price", MySqlDbType.Decimal).Value = price;
+            command.Parameters.Add("@total_price", MySqlDbType.Decimal).Value = total;
+
+            //adapter.SelectCommand = command;
+
+
+            //adapter.Fill(table);
+
+            //Open the connection
+            db.OpenConnection();
+            command.ExecuteNonQuery();
+
+            //close the connection
+            db.CloseConnection();
+        }
+
+        [Route("AddtoOrderDB")]
+        [HttpPut]
+        public void AddToOrderDB(string name, int qty, decimal price, decimal total)
+        {
+
+            //---------//
+
+            //Add new user
+            DB db = new DB();
+            MySqlCommand command = new MySqlCommand("INSERT INTO `orders` (`ItemName`, `Qty`, `Price`, `TotalPrice`) VALUES (@itn, @qty, @price, @total_price)", db.GetConnection());
+
+            //command.Parameters.Add("@usn", MySqlDbType.VarChar).Value = textBoxUsername.Text;
+            //command.Parameters.Add("@pass", MySqlDbType.VarChar).Value = textBoxPassword.Text;
+            //command.Parameters.Add("@fn", MySqlDbType.VarChar).Value = textBoxFirstName.Text;
+            //command.Parameters.Add("@ln", MySqlDbType.VarChar).Value = textBoxLastName.Text;
+            //command.Parameters.Add("@email", MySqlDbType.VarChar).Value = textBoxEmail.Text;
+
+
+            // command.Parameters.Add("@usn", MySqlDbType.VarChar).Value = GetUser();
+            command.Parameters.Add("@itn", MySqlDbType.VarChar).Value = name;
+            command.Parameters.Add("@qty", MySqlDbType.Int32).Value = qty;
+            command.Parameters.Add("@price", MySqlDbType.Decimal).Value = price;
+            command.Parameters.Add("@total_price", MySqlDbType.Decimal).Value = total;
+
+            //adapter.SelectCommand = command;
+
+
+            //adapter.Fill(table);
+
+            //Open the connection
+            db.OpenConnection();
+            command.ExecuteNonQuery();
+
+            //close the connection
+            db.CloseConnection();
+        }
+
     }
 }
