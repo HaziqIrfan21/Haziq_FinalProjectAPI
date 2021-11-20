8b2a70e5364d3cb2e79996be8ea2bafca03d5c93
HaziqIrfan21 <90903576+HaziqIrfan21@users.noreply.github.com>
2021-11-20 09:22:23 +0800

    Added back home controller

---
+++ RestaurantAPI/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using EFDataAcessLibrary.DataAccess;
+using EFDataAcessLibrary.Models;
+using Haziq_FinalProject;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestaurantAPI.Models;
+
+namespace RestaurantAPI.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly RestaurantContext _db;
+
+        public HomeController(ILogger<HomeController> logger, RestaurantContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        public void OnGet()
+        {
+            LoadSampleData();
+
+            //var people = _db.User.ToList();
+        }
+
+        private void LoadSampleData()
+        {
+            if (_db.User.Count() == 0)
+            {
+                string file = System.IO.File.ReadAllText("users.json");
+                var user = JsonSerializer.Deserialize<List<Users>>(file);
+                _db.AddRange(user);
+                _db.SaveChanges();
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
